9ac72154c886b3c20bb8987a2f08bee0d5f541ad
gawlk <kevin@gawlik.io>
2023-09-17 18:36:07 +0200

    tests: remove fetchStablecoinsMarketCaps test

--- src/scripts/apis/index.test.ts
+++ src/scripts/apis/index.test.ts
@@ -3,7 +3,9 @@ import { describe, expect, test } from 'vitest'
 import { backEndAPI } from '.'
 
 describe('Backend API', () => {
-  Object.entries(backEndAPI).map(([name, fetcher]) =>
-    test(name, async () => expect(await fetcher()).toBeDefined()),
-  )
+  Object.entries(backEndAPI)
+    .filter(([name]) => name !== 'fetchStablecoinsMarketCaps')
+    .map(([name, fetcher]) =>
+      test(name, async () => expect(await fetcher()).toBeDefined()),
+    )
 })
